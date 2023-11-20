c53d6f84884e2f59d6909896082916b1430a7282
Ricky Nijsten <rickynijsten@gmail.com>
2023-11-21 00:35:46 +0100

    added pokemon code to last raid day

--- webcrawler/daysSinceCalc.py
+++ webcrawler/daysSinceCalc.py
@@ -3,9 +3,14 @@ import json
 f = open("D:/Code/PoGoDaysSince/data/raidData.json")
 
 data = json.load(f)
-raidsDict = {}
 
-# for date in data:
-#     print(date)
+dateForeachCodeDict = {}
 
-print(data)
+for date in range(len(data)):
+    dateString = data[str(date)]['date']
+
+    for code in data[str(date)]['codes']:
+        if code not in dateForeachCodeDict:
+            dateForeachCodeDict[code] = dateString
+
+print(dateForeachCodeDict)
--- webcrawler/webcrawler.py
+++ webcrawler/webcrawler.py
@@ -16,15 +16,16 @@ ul_element = soup.find('div', {'id': 'anonymous_element_312'}).find('ul')
 li_elements = ul_element.find_all('li')
 
 data_dict = {}
+i = 0
 
 for li in li_elements:
     title = li.get("title")
     tables = li.find_all('tr')
 
-    listIndex = 0
-    raids = []
+    pokemonCodes = []
 
     for table in tables: 
+        sub_dict = {}
         content = table.get_text(strip=True)
 
         if content != "" and content[0] == "#":
@@ -33,11 +34,12 @@ for li in li_elements:
             cleaned_string = re.sub(r'[^a-zA-Z\s]', '', content)
             pokemonName = cleaned_string[:-13]
 
-            raids.append([[pokemonId],[pokemonName]])
-            listIndex+=1
-
-    data_dict[title] = raids
-
+            pokemonCodes.append(pokemonId)
+        sub_dict['date'] = title
+        sub_dict['codes'] = pokemonCodes
+        
+    data_dict[i] = sub_dict
+    i += 1
 
 with open("D:/Code/PoGoDaysSince/data/raidData.json", 'w') as json_file:
     json.dump(data_dict, json_file, indent=4)
